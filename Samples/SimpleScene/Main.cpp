ce31139005a06b4ca3a1e2d2491e4017cc136ffe
Constantine Tarasenkov <iamvfx@gmail.com>
2022-06-21 05:09:41 +0300

    Load simple scene from C:/anki/main.lua

--- Samples/SimpleScene/Main.cpp
+++ Samples/SimpleScene/Main.cpp
@@ -14,7 +14,7 @@ public:
 	Error sampleExtraInit()
 	{
 		ScriptResourcePtr script;
-		ANKI_CHECK(getResourceManager().loadResource("Assets/Scene.lua", script));
+		ANKI_CHECK(getResourceManager().loadResource("C:/anki/main.lua", script));
 		ANKI_CHECK(getScriptManager().evalString(script->getSource()));
 
 		return Error::NONE;
